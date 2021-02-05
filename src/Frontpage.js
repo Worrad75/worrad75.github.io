cf8f8876e304c5ae274be1edd217b3a9be3228cd
github username <github email address>
2021-02-04 20:54:16 -0500

    created and styled interactive greeting buttons

--- src/Frontpage.js
+++ src/Frontpage.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import { Component } from 'react'
 import './animation.css'
+import Line from './Line'
 
 export default class Frontpage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text : "Hi, my name is darrow",
-            count : 0,
+            text: ["Hi, my name is darrow", "that's right, keep clicking", "I'm a software engineer", "come, let me show you..."],
+            count : -1,
             nextText : []
         };
     }
 
+// when we hit the last 'slide', the text progressing button will disappear and the user will get 2 new buttons:
+//      "who I am" and "what I've been up to", which will lead to about me and projects sections respectively
+
+
     changeTypedWording = () => {
-        this.setState({ text: "follow me" });
-    }
+        if(this.state.count < 3) {
+            this.setState({ count: this.state.count += 1 })
+            const elem = document.getElementById('type-written')
+            if(elem) elem.parentNode.removeChild(elem);
+
+            const test = document.createElement('h1')
+            test.id = 'type-written'
+            const newText = this.state.text[this.state.count]
+            test.innerText = newText
+            test.style.maxWidth = newText.length * 5
+            document.getElementById("typewriter").appendChild(test)
+        } else {
+            this.setState({ count: this.state.count += 1 })
+            const buttonToRemove = document.getElementById("text-carousel-button");
+            buttonToRemove.parentElement.removeChild(buttonToRemove)
+        }
+    }   
 
     render() {
-
         return (
-            <div className="frontpage" id="typewriter">
-                <h1 onMouseDown={this.changeTypedWording}>{this.state.text}</h1>
+            <div className="frontpage">
+                <div id="typewriter">
+                    {/* <h1 id="type-written">{this.state.text[0]}</h1> */}
+                </div>
+                
+                    <button id="text-carousel-button" onMouseDown={this.changeTypedWording}>click me!</button>
+                
+                {this.state.count > 3 ? <Line></Line> : <div></div>}
             </div>)
     }
 }
---
+++ src/Line.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export default function Line() {
+
+    return <div className="content">
+        <div className="line">
+            <div id="starter-buttons">
+                <button className="optionButton">who I am</button>
+                <button className="optionButton">what I'm up to</button>
+            </div>
+        </div>
+    </div>
+
+}
