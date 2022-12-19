a8f682445033e90ae1a0743acd7d365845d9b133
github username <github email address>
2022-12-19 17:03:25 -0500

    cleaning house

--- src/Frontpage.js
+++ src/Frontpage.js
@@ -7,49 +7,10 @@ export default class Frontpage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            text: ["Hi, my name is Darrow.", "Please keep clicking.", "I'm a Software Engineer!", "come, let me show you..."],
-            count : -1,
-            nextText : [],
-            placeholder: true,
         };
-    }
-
-// when we hit the last 'slide', the text progressing button will disappear and the user will get 2 new buttons:
-//      "who I am" and "what I've been up to", which will lead to about me and projects sections respectively
-
-
-    changeTypedWording = () => {        
-        if (this.state.count < this.state.text.length - 1) {
-            const newCount = this.state.count + 1
-            this.setState({ count: newCount, placeholder: false })
-            const elem = document.getElementById('type-written')
-            if(elem) elem.parentNode.removeChild(elem);
-
-            const freshTypewritter = document.createElement('h1')
-            freshTypewritter.id = 'type-written'
-            const newText = this.state.text[newCount]
-            freshTypewritter.innerText = newText
-            freshTypewritter.style.maxWidth = newText.length * 5
-            document.getElementById("typewriter").appendChild(freshTypewritter)
-        } else {
-            const newCount = this.state.count + 1
-            this.setState({ count: newCount })
-            const buttonToRemove = document.getElementById("text-carousel-button");
-            buttonToRemove.parentElement.removeChild(buttonToRemove)
-        }
-    }   
+    }  
 
     render() {
-        return (
-            <div className="frontpage">
-
-                <div id="typewriter"></div>
-                {this.state.placeholder ? <div id="placeholder"></div> : <></>}
-                
-                <button id="text-carousel-button" onMouseDown={this.changeTypedWording}>click me!</button>
-                
-                {this.state.count > this.state.text.length -1 ? <Options></Options> : <div></div>}
-
-            </div>)
+        return (<div></div>)
     }
-}
+} 
--- src/Projects.js
+++ src/Projects.js
@@ -4,44 +4,6 @@ import { Controller, Scene } from 'react-scrollmagic';
 export default function Projects() {
 
     return <section className="about">
-
-        <div className="about-pages">
-
-            <div>
-                <h2>Algorithm Visualizer</h2>
-                <p>Lorem Ipsum është një tekst shabllon i industrisë
-                së printimit dhe shtypshkronjave. Lorem Ipsum ka
-                qenë teksti shabllon i industrisë që nga vitet 1500
-                , kur një shtypës i panjohur morri një galeri shkrimesh
-                dhe i ngatërroi për të krijuar një libër mostër.</p>
-            </div>
-            <div>
-                <h2>Project 2</h2>
-                <p>Lorem Ipsum është një tekst shabllon i industrisë
-                së printimit dhe shtypshkronjave. Lorem Ipsum ka
-                qenë teksti shabllon i industrisë që nga vitet 1500
-                , kur një shtypës i panjohur morri një galeri shkrimesh
-                dhe i ngatërroi për të krijuar një libër mostër.</p>
-            </div>
-            <div>
-                <h2>Project 3</h2>
-                <p>Lorem Ipsum është një tekst shabllon i industrisë
-                së printimit dhe shtypshkronjave. Lorem Ipsum ka
-                qenë teksti shabllon i industrisë që nga vitet 1500
-                , kur një shtypës i panjohur morri një galeri shkrimesh
-                dhe i ngatërroi për të krijuar një libër mostër.</p>
-            </div>
-
-        </div>
-
-        <Controller>
-            <Scene duration={1800} pin=".projects-title">
-                <div className="projects-title">
-                    <h2> PROJECTS </h2>
-                </div>
-            </Scene>
-        </Controller>
-
     </section>
 
 }
