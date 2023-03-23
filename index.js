577d4b8776950cb1d0d966366dedf69c1f60028d
Darrow Palast <darrow@Darrows-MacBook-Pro.local>
2023-03-23 16:45:06 -0400

    got animation working, need to line up landing page then build out info pages

--- index.js
+++ index.js
@@ -1,16 +1,39 @@
-// import * as THREE from './three.js-master/build/three.module.js'
-// import * as THREE from "../node_modules/three/build/three.module.js"
-// import * as THREE from "../three.js-master/build/three.module.js"
-import * as THREE from 'three';
-import { GLTFLoader } from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
+// import { Scene, WebGLRenderer, PerspectiveCamera, DirectionalLight } from 'three';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+// import {  } from 'three/examples/jsm/loaders/GLTFLoader';
 
-const canvas = document.querySelector('#webgl')
 const scene = new THREE.Scene()
+const loader = new THREE.GLTFLoader();
+// const controls = new THREE.OrbitControls();
+let mixer;
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
 
-const loader = new GLTFLoader()
-loader.load('./assets/boat/boat.glb', function(glb){
-    glb.scene.scale.set(0.05,0.05,0.05)
+// Sets a 12 by 12 gird helper
+// const gridHelper = new THREE.GridHelper(12, 12);
+// scene.add(gridHelper);
+
+// Sets the x, y, and z axes with each having a length of 4
+// const axesHelper = new THREE.AxesHelper(4);
+// scene.add(axesHelper);
+
+
+loader.load('./public/assets/boat/boat.glb', function(glb){
+    const model = glb.scene
+    model.rotation.y = Math.PI / 2;
+    model.scale.set(1,1,1)
+    scene.add(model)
+    mixer = new THREE.AnimationMixer(model)
+    const clips = glb.animations
+    const clip = THREE.AnimationClip.findByName(clips, "Sailing")
+    const action = mixer.clipAction(clip)
+    action.play()
+
     console.log(glb)
+
 }, function(xhr){
     console.log(xhr.loaded/xhr.total * 100 + "% loaded")
 }, function(error){
@@ -19,30 +42,30 @@ loader.load('./assets/boat/boat.glb', function(glb){
 
 const light = new THREE.DirectionalLight(0xffffff, 1)
 light.position.set(2,2,5)
-// light.position.set(0,1,0)
 scene.add(light)
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
-const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 100)
-camera.position.set(0,1,2)
+const camera = new THREE.PerspectiveCamera(90, sizes.width/sizes.height, 0.1, 100)
+camera.position.set(0,1,3)
 scene.add(camera)
 
-const renderer = new THREE.WebGL1Renderer({
+const canvas = document.querySelector('#webgl')
+const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
-
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.shadowMap.enabled = true
-renderer.gammaOutput = true
+renderer.gammaOutput = true   
+const clock = new THREE.Clock()
+renderer.setAnimationLoop(animate)
 
 function animate() {
-    requestAnimationFrame(animate)
+    mixer.update(clock.getDelta())
     renderer.render(scene, camera)
 }
 
-animate()
+window.addEventListener('resize', function() {
+    camera.aspect = sizes.width/sizes.height
+    camera.updateProjectionMatrix();
+    renderer.setSize(sizes.width/sizes.height)
+})
